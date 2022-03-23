7eaeae6495feac084cbcd33a719d52094708f52e
ttaka66 <d.m_ro.nl.e@icloud.com>
2022-03-24 01:59:07 +0900

    impl chapter04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,41 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// NOTE: I don't know how to make sure it works correctly.
+
+@SCREEN
+  D=A
+  @screen_address
+  M=D
+  @KBD
+  D=M
+  @BLACK
+  D;JNE
+(WHITE)
+  @screen_address
+  D=M
+  @24575
+  D=D-A
+  @0
+  D;JGE
+  @screen_address
+  A=M
+  M=0
+  @screen_address
+  M=M+1
+  @WHITE
+  0;JMP
+(BLACK)
+  @screen_address
+  D=M
+  @24575
+  D=D-A
+  @0
+  D;JGE
+  @screen_address
+  A=M
+  M=-1
+  @screen_address
+  M=M+1
+  @BLACK
+  0;JMP
